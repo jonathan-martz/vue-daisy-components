b47cafad092d7645336a2034a2fdafa752ac9b3c
jonathanm <git@jmartz.de>
2026-07-12 12:44:57 +0200

    update version number for headline

--- components/Headline.vue
+++ components/Headline.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="headline flex justify-between">
-    <h2 class="font-bold text-lg mb-3">
+  <section class="headline flex justify-between mb-3 items-center">
+    <h2 class="font-bold text-lg">
       <slot />
     </h2>
     <section class="actions space-x-3">
