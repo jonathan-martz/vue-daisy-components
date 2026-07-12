4fba566f0e41e73de6c32881263c3bd9571c70a6
jonathanm <git@jmartz.de>
2026-07-12 12:40:58 +0200

    add custom component headline

---
+++ components/Headline.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="headline flex justify-between">
+    <h2 class="font-bold text-lg mb-3">
+      <slot />
+    </h2>
+    <section class="actions space-x-3">
+      <slot name="actions" />
+    </section>
+  </section>
+</template>
